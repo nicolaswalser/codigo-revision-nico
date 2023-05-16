74b9c19ab0de179a4d7a20fd6c5aa81e2bf59564
Nicolas <walsergomez@gmail.com>
2023-05-16 13:07:36 -0600

    ci: push revisions

---
+++ codigo-otros-2/formulario.js
@@ -0,0 +1,87 @@
+// Esta aplicación crea una lista de invitados.
+// Los invitados pueden ser agregados a través de un formulario
+// Se pueden borrar invitados de la lista usando el botón
+
+var formulario = document.querySelector(".formulario");
+
+formulario.onsubmit = function (e) {
+  e.preventDefault(); // prevent() to preventDefault()
+
+  var n = formulario.elements[0];
+  var e = formulario.elements[1];
+  var na = formulario.elements[2];
+
+  var nombre = n.value;
+  var edad = e.value;
+
+  var i = na.selectedIndex;
+  var nacionalidad = na.options[i].value;
+  console.log(nombre, edad);
+  console.log(nacionalidad);
+
+  if (nombre.length === 0) {
+    n.classList.add("error");
+  }
+  if (edad <= 18 || edad >= 120) {
+    e.classList.add("error");
+  }
+
+  if (nombre.length >= 0 && edad >= 18 && edad <= 120) {
+    // equal to
+    agregarInvitado(nombre, edad, nacionalidad);
+  }
+};
+
+var botonBorrar = document.createElement("button");
+botonBorrar.textContent = "Eliminar invitado";
+botonBorrar.id = "boton-borrar";
+var corteLinea = document.createElement("br");
+document.body.appendChild(corteLinea);
+document.body.appendChild(botonBorrar);
+
+function agregarInvitado(nombre, edad, nacionalidad) {
+  if (nacionalidad === "ar") {
+    nacionalidad = "Argentina";
+  } else if (nacionalidad === "mx") {
+    nacionalidad = "Mexicana";
+  } else if (nacionalidad === "vnzl") {
+    nacionalidad = "Venezolana";
+  } else if (nacionalidad === "per") {
+    nacionalidad = "Peruana";
+  }
+
+  var lista = document.getElementById("lista-de-invitados");
+
+  var elementoLista = document.createElement("div");
+  elementoLista.classList.add("elemento-lista");
+  lista.appendChild(elementoLista);
+
+  var spanNombre = document.createElement("span");
+  var inputNombre = document.createElement("input");
+  var espacio = document.createElement("br");
+  spanNombre.textContent = "Nombre: ";
+  inputNombre.value = nombre;
+  elementoLista.appendChild(spanNombre);
+  elementoLista.appendChild(inputNombre);
+  elementoLista.appendChild(espacio);
+
+  function crearElemento(descripcion, valor) {
+    var spanNombre = document.createElement("span");
+    var inputNombre = document.createElement("input");
+    var espacio = document.createElement("br");
+    spanNombre.textContent = descripcion + ": ";
+    inputNombre.value = valor;
+    elementoLista.appendChild(spanNombre);
+    elementoLista.appendChild(inputNombre);
+    elementoLista.appendChild(espacio);
+  }
+
+  crearElemento("Nombre", nombre);
+  crearElemento("Edad", edad);
+  crearElemento("Nacionalidad", nacionalidad);
+
+  botonBorrar.onclick = function () {
+    elementoLista.remove();
+    // deleted unnecessary code, only placed     elementoLista.remove(); onclick
+  };
+}
